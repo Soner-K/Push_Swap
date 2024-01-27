d929169cbc70e3f53f221c9c35d0838b124f96d8
sokaraku <soner.karakus1999@gmail.com>
2024-01-27 21:51:42 +0100

    Forgot to delete some stuff in the main

--- src/sorting/main.c
+++ src/sorting/main.c
@@ -6,7 +6,7 @@
 /*   By: sokaraku <sokaraku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 18:27:26 by sokaraku          #+#    #+#             */
-/*   Updated: 2024/01/27 21:36:20 by sokaraku         ###   ########.fr       */
+/*   Updated: 2024/01/27 21:50:14 by sokaraku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ int	main(int ac, char **av)
 	t_node		*stack_a;
 	t_node		*stack_b;
 	t_sizes		sizes;
-	char		str[1000];
 
-	str[999] = 0;
 	stack_a = create_list(ac, av, 0);
 	if (!stack_a)
 		return (1);
@@ -29,8 +27,6 @@ int	main(int ac, char **av)
 	sizes.lstsize_a = ft_lstsize(stack_a);
 	sizes.lstsize_b = 0;
 	sort(&stack_a, &stack_b, &sizes);
-	read(0, str, 1000);
-	printf("commandes %s\n", str);
 	clear_list(&stack_a);
 	clear_list(&stack_b);
 }
